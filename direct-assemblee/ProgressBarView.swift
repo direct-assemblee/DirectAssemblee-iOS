44c1bdfcef6376639a53875aff5f2d303dfc283c
Julien Coudsi <julien.coudsi@backelite.com>
2018-10-10 12:31:42 +0200

    Improves code for ProgressBarView

--- direct-assemblee/ProgressBarView.swift
+++ direct-assemblee/ProgressBarView.swift
@@ -9,16 +9,15 @@
 
 import UIKit
 
-class ProgressBarView: UIView {
+@IBDesignable class ProgressBarView: UIView {
 
-    var progress: Float = 0
-    var progressTintColor = UIColor(hex: Constants.Color.blueColorCode)
-    var trackTintColor = UIColor(hex: Constants.Color.blueLightColorCode)
-    
+    @IBInspectable var progress: Float = 0
+    @IBInspectable var progressTintColor: UIColor = UIColor(hex: Constants.Color.blueColorCode)
+    @IBInspectable var trackTintColor: UIColor = UIColor(hex: Constants.Color.blueLightColorCode)
     
     private var trackLayer: CAShapeLayer {
         
-        let trackLayer =  self.getBarLayer(width: self.bounds.width)
+        let trackLayer =  self.getBarLayer(width: self.bounds.size.width)
         trackLayer.fillColor = self.trackTintColor.cgColor
         
         return trackLayer
@@ -26,12 +25,13 @@ class ProgressBarView: UIView {
     
     private var progressLayer: CAShapeLayer {
         
-        let width = CGFloat(self.progress) * self.bounds.width
+        let width = CGFloat(self.progress) * self.bounds.size.width
         let progressLayer =  self.getBarLayer(width: width)
         progressLayer.fillColor = self.progressTintColor.cgColor
         
         return progressLayer
     }
+
 
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -45,13 +45,16 @@ class ProgressBarView: UIView {
         
         let progressLayer = self.progressLayer
         self.layer.addSublayer(progressLayer)
-        self.animatesLayer(progressLayer)
+        
+        #if !TARGET_INTERFACE_BUILDER
+            self.animatesLayer(progressLayer)
+        #endif
     }
     
 
     private func getBarLayer(width: CGFloat) -> CAShapeLayer {
         
-        let rect = CGRect(x: self.bounds.minX, y: self.bounds.minY, width: width, height: self.bounds.height)
+        let rect = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: width, height: self.bounds.size.height)
         let path = UIBezierPath(roundedRect: rect, cornerRadius: 5)
         
         let layer = CAShapeLayer()
@@ -64,7 +67,7 @@ class ProgressBarView: UIView {
     
     private func animatesLayer(_ layer: CAShapeLayer) {
     
-        let fromRect = CGRect(x: self.bounds.minX, y: self.bounds.minY, width: 0, height: self.bounds.height)
+        let fromRect = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: 0, height: self.bounds.size.height)
         let fromPath = UIBezierPath(roundedRect: fromRect, cornerRadius: 5).cgPath
         let toPath = layer.path
         
